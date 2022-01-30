44687bec7546a46a47c79322adbcc2c976ad8b86
whes1015 <44525760+whes1015@users.noreply.github.com>
2022-01-30 23:16:13 +0800

    Add files via upload

--- src/main/java/eventlistener/eventlistener.java
+++ src/main/java/eventlistener/eventlistener.java
@@ -1,7 +1,12 @@
 package eventlistener;
 
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 
 public class eventlistener implements Listener {
-
+    @EventHandler
+    public void PlayerJoinEvent(PlayerJoinEvent event){
+        event.getPlayer().sendMessage("\u524d\u5f80 https://exptechweb.mywire.org/ \u53ef\u4ee5\u8cfc\u8cb7\u7269\u8cc7");
+    }
 }
